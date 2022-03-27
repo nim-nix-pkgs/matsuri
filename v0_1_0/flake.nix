20233fad08326d3958b6c90d72f42f5cb426a3d1
hugosenari <hugosenari@gmail.com>
2022-03-27 18:20:22 -0300

    feat(nimpkgs): remove lock

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-matsuri-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-matsuri-v0_1_0.owner = "zer0-star";
   inputs.src-matsuri-v0_1_0.repo  = "matsuri";
+  inputs.src-matsuri-v0_1_0.dir   = "";
   inputs.src-matsuri-v0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
